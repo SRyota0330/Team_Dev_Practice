3f842294e722a21e9d2b53dd3b4585efac40f8d7
banana <tchbn@example.com>
2025-07-17 15:34:03 +0900

    I and MASSUN edited CartService.java

--- src/main/java/com/example/demo/service/CartService.java
+++ src/main/java/com/example/demo/service/CartService.java
@@ -1,5 +1,35 @@
 package com.example.demo.service;
 
-public class CartService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Item;
+import com.example.demo.entity.Order;
+import com.example.demo.entity.OrderItem;
+import com.example.demo.repository.OrderItemRepository;
+
+@Service
+public class CartService {
+	
+	@Autowired
+	OrderItemRepository orderItemRepository;
+	
+	public void addItemToCart(Order order, Item item) {
+		orderItemRepository.addItemToCart(order, item);
+	}
+	
+	public void delItemFromCart(int orderId, int itemId) {
+		orderItemRepository.delItemFromCart(orderId, itemId);
+	}
+	
+	public void editItemOnCart(int quantity) {
+		orderItemRepository.editItemOnCart(quantity);
+	}
+	
+	public List<OrderItem> getAllItems(OrderItem orderItem){
+		return orderItemRepository.getAllItems(orderItem);
+	}
+	
 }
