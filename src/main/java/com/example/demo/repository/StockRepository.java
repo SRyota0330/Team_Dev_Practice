cd715840e2e69017bc1f11cf59a32956987740aa
Ryota <ryota@example.com>
2025-07-17 14:22:05 +0900

    create stockRepository & stockService

--- src/main/java/com/example/demo/repository/StockRepository.java
+++ src/main/java/com/example/demo/repository/StockRepository.java
@@ -1,5 +1,20 @@
 package com.example.demo.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Item;
+
+@Repository
 public class StockRepository {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public void manageCount(int count,Item item) {
+	    String query = "UPDATE stock SET count = ?, WHERE item_id = ?";
+	    jdbcTemplate.update(query,count,item);
+	}
 
 }
+
--- src/main/java/com/example/demo/service/StockService.java
+++ src/main/java/com/example/demo/service/StockService.java
@@ -1,5 +1,18 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Item;
+import com.example.demo.repository.StockRepository;
+
+@Service
 public class StockService {
+	@Autowired
+	StockRepository stockRepository;
+	
+	public void manageCount(int count,Item item) {
+		stockRepository.manageCount(count,item);
+	}
 
 }
