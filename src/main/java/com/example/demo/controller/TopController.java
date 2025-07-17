ccb6c1f542cc98e61a124fea705928e9fb0d68de
Ryota <ryota@example.com>
2025-07-17 10:47:25 +0900

    create item service and repository

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -1,15 +1,25 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Item;
+import com.example.demo.service.ItemService;
+
 @Controller
 public class TopController {
+    @Autowired
+    private ItemService itemService;
+    
 	@GetMapping(value="/")
-	public String index(Model model) {
-		model.addAttribute("message","Hello World");
-		return "index";
+	public String toppage(Model model) {
+		List<Item> itemList = itemService.getAllItem();
+		model.addAttribute("items",itemList);
+		return "top";
 	}
 
 }
