ce3f5bfa0fdc52b2a70b9ffaa49b2ecdce0d3c84
banana <tchbn@example.com>
2025-07-24 14:58:09 +0900

    レジに進む処理

--- src/main/java/com/example/demo/controller/PurchaseController.java
+++ src/main/java/com/example/demo/controller/PurchaseController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.Item;
 import com.example.demo.entity.Order;
@@ -25,18 +24,18 @@ public class PurchaseController {
 	
 	//レジに進むメソッド
 	@GetMapping(value="/checkout")
-	public String moveToCashRegister(Model model) {
-		return "purchase/cashRegister";
-	}
-	
-	@PostMapping(value="/checkout")
 	public String checkout(HttpSession session, Model model) {
 		//セッションからログインユーザーを取得
-		User loginUser = (User) session.getAttribute("loginUser");
-		Long userId = loginUser.getUserid();
+		Long userid = (Long) session.getAttribute("userid");
+		
+		System.out.println("ユーザIDは"+userid);
+		
+		User loginUser = userService.userDetail(userid);
 		
 		//ユーザーの住所を取得
-		String address = userService.getAddressFromUser(userId);
+		System.out.println("test");
+		String address = userService.getAddressFromUser(userid);
+		System.out.println("test");
 		
 		//注文を取得
 		Order order = loginUser.getOrder();
@@ -56,7 +55,7 @@ public class PurchaseController {
 		model.addAttribute("total", total);
 		model.addAttribute("address", address);
 		
-		return "cashRegister";
+		return "purchase/cashRegister";
 	}
 	
 	//カートの中身個数を調整するメソッド
