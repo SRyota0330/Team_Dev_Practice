ab5da34f87a7b09fd721c7a12b0fc523407de5a0
banana <tchbn@example.com>
2025-07-25 11:38:02 +0900

    購入完了ボタンの実装

--- src/main/java/com/example/demo/controller/PurchaseController.java
+++ src/main/java/com/example/demo/controller/PurchaseController.java
@@ -8,18 +8,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.Item;
 import com.example.demo.entity.Order;
 import com.example.demo.entity.OrderItem;
 import com.example.demo.entity.User;
 import com.example.demo.service.CartService;
+import com.example.demo.service.StockService;
 import com.example.demo.service.UserService;
  
 @Controller
 public class PurchaseController {
+	
 	@Autowired CartService cartService;
 	@Autowired UserService userService;
+	@Autowired StockService stockService;
+	
 	//レジに進むメソッド
 	@GetMapping(value="/checkout")
 	public String checkout(HttpSession session, Model model) {
@@ -31,7 +36,7 @@ public class PurchaseController {
 		System.out.println("test");
 		String address = userService.getAddressFromUser(userid);
 		System.out.println("test");
-		//注文を取得
+		//ユーザーのorderテーブルを取得
 		Order order = loginUser.getOrder();
 		//商品情報を取得
 		List<OrderItem> orderItems = cartService.getAllItems(order.getOrderid());
@@ -50,4 +55,37 @@ public class PurchaseController {
 	//カートの中身個数を調整するメソッド
 
 	//カートから削除するメソッド
+	
+	//購入確定ボタン
+	@GetMapping(value="/confirm")
+	public String finishShopping(Model model) {
+		return "purchase/purchased";
+	}
+	
+	@PostMapping(value="/confirm")
+	public String confirm(HttpSession session, Model model){
+		//セッションからログインユーザーを取得
+		Long userid = (Long) session.getAttribute("userid");
+		User loginUser = userService.userDetail(userid);
+		
+		//ユーザーのordersテーブルを取得
+		Order order = loginUser.getOrder();
+		
+		//ordersテーブルに紐ついているOrderItemテーブル（注文の中身）を取得
+		List<OrderItem> orderItems = cartService.getAllItems(order.getOrderid());
+		
+		//カートのアイテムの数の分だけ「在庫数-注文個数=残り在庫数」を実施
+		for(OrderItem oi : orderItems) {
+			int count = 0;
+			count = stockService.getCount(oi.getItem().getItemid()); //在庫数の取得
+			int quantity = oi.getQuantity(); //注文個数の取得
+			
+			int reststock = count - quantity;
+			
+			stockService.manageCount(reststock, oi.getItem()); //StockServiceのmanageCountメソッドに渡す
+		}
+		
+		//購入完了ページに遷移
+		return "purchase/purchased";
+	}
 }
